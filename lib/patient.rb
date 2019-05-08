433f93ddec547abea874a3fe4ea8865ae39b2780
BethAnn Hanlon <herbivore137@gmail.com>
2019-05-08 02:55:10 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -9,17 +9,14 @@ class Patient
     @@all << self
   end
 
-  # given a doctor and date, creates a new appointment belonginng to that patient
   def new_appointment(doctor, date)
     Appointment.new(date, self, doctor)
   end
   
-  # has many appointments
   def appointments
     Appointment.all.select{|a| a.patient == self}
   end
   
-  # has many doctors through appointments
   def doctors
     appointments.collect{|a| a.doctor}
   end
