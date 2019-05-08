a442fe212be3ec588a26a31d3626090e2d99faf2
BethAnn Hanlon <herbivore137@gmail.com>
2019-05-08 02:49:26 +0000

    Automatically backed up by Learn

---
+++ lib/doctor.rb
@@ -0,0 +1,31 @@
+class Doctor
+  
+  attr_accessor :name
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  # given a date and a patient, creates a new appointment
+  def new_appointment(date, patient)
+    Appointment.new(date, patient, self)
+  end
+  
+  # has many appointments
+  def appointments
+    Appointment.all.select{|a| a.doctor == self}
+  end
+  
+  # has many patients, through appointments
+  def patients
+    appointments.collect{|a| a.patient}
+  end
+  
+  def self.all
+    @@all
+  end
+  
+end
